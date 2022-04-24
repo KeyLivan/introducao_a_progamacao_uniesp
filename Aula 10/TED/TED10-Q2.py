8fb4c87d2dc723519163b2bb22c238b04126867c
João Gabriel G.S <102331350+KeyLivan@users.noreply.github.com>
2022-04-24 00:50:46 -0300

    Update TED10-Q2.py

--- Aula 10/TED/TED10-Q2.py
+++ Aula 10/TED/TED10-Q2.py
@@ -21,4 +21,4 @@ for chave, valor in enumerate(VET):
 
 for valor in chaves:
     if len(chaves[valor]) > 1:
-        print(valor, chaves[valor])
+        print(f'O numero {valor} se repetede nos indecis {chaves[valor]}')
