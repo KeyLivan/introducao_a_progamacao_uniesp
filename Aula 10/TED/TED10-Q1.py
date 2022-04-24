fea99dad3664fad42955204ab84104e295fb0fbe
João Gabriel G.S <102331350+KeyLivan@users.noreply.github.com>
2022-04-24 00:24:17 -0300

    Add files via upload

---
+++ Aula 10/TED/TED10-Q1.py
@@ -0,0 +1,9 @@
+# Faça um algoritmo para ler 20 números e armazenar em um vetor. Após a leitura total dos 20 números, o algoritmo deve escrever esses
+# 20 números lidos na ordem inversa.
+
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+numeros.reverse()
+
+print(numeros)
---
+++ Aula 10/TED/TED10-Q2.py
@@ -0,0 +1,24 @@
+# Faça um algoritmo para ler 50 números e armazenar em um vetor VET, verificar e escrever se existem números repetidos no vetor VET
+# e em que posições se encontram.
+
+from collections import defaultdict
+import random
+
+
+VET = []
+
+for x in range(1, 51):
+    NumAle = random.randrange(1, 50)
+    VET.append(NumAle)
+
+print(VET)
+print('-' * 100)
+
+chaves = defaultdict(list)
+
+for chave, valor in enumerate(VET):
+    chaves[valor].append(chave)
+
+for valor in chaves:
+    if len(chaves[valor]) > 1:
+        print(valor, chaves[valor])
