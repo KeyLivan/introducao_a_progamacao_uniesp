f3e4169a92ef984bd69ed5273b1805cd36d0be03
João Gabriel G.S <102331350+KeyLivan@users.noreply.github.com>
2022-04-28 20:55:17 -0300

    Add files via upload

---
+++ Aula 11/exemplo02.py
@@ -0,0 +1,14 @@
+import random
+
+m = []
+
+for x in range(1, 11):
+    lista_auxiliar = []
+    for y in range(1, 11):
+        lista_auxiliar.append(random.randint(1, 1000))
+    print('--- Adicionando Na Matriz ---')
+    print(lista_auxiliar)
+    m.append(lista_auxiliar)
+
+print('--- Nossa Matriz ---')
+print(m)
